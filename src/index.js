6c86e4a5e7ec74f2b5697076a295faf6f4b81061
Eduardo Garcia <conejo1542@gmail.com>
2026-02-11 08:56:37 -0600

    end point de resta

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const app = express();
 const morgan= require ('morgan');//muestra las solicitudes http get pot put patch en la consola
 const cors = require ('cors');//permite conexiones externas al servidor
 
+
 //configuracion del servidor puerto 3000
 
 app.set('port',process.env.PORT || 3000);
-app.set('json spaces',2)
+app.set('json spaces',2)//HABILITAMOS JSON
+
 
 app.use(morgan('dev'));//cargar morgan maneja errores
 
@@ -16,15 +18,16 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());// manejar el formato Json
 app.use(cors());// permitir conexiones externas  
 
-console.log ("hola mundo ")
+
 
 app.get('/', (req, res) => {    
     res.json(
-        {
-            "Title": "Hola mundo"
+        { 
+            "Title": "Hola mundo esta es mi primer api"
         }
     )
 });
+
 
 //comentario ejemplo
 app.post('/sumar', (req, res) => { // http://localhost:3000/sumar
@@ -43,9 +46,25 @@ app.post('/sumar', (req, res) => { // http://localhost:3000/sumar
   res.send({ resultado });
 });
 
+app.post('/restar', (req, res) => { // http://localhost:3000/sumar
+  const { num1, num2 } = req.body;// se declaran los datos de entrada
 
+  // Validar que se hayan enviado los dos números  que no esten vacio
+  if (!num1 || !num2) {
+    return res.status(400).send({ error: 'Faltan números para sumar' });
+  }
+
+  // Sumar los números
+  const resultado = num1 - num2;
+
+  // Enviar el resultado al front 
+
+  res.send({ resultado });
+});
 //Iniciando el servidor
+
+
 app.listen(app.get('port'),()=>{
-    console.log(`Server listening on port ${app.get('port')}`);
+    console.log("SERVIDOR EN EL PUERTO 3000");
 });
 
