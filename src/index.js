236d66a9ad18c16c70482779f95edd8aec0bc36f
Eduardo Garcia <conejo1542@gmail.com>
2026-02-13 08:29:38 -0600

    endpint de area del triangulo

--- src/index.js
+++ src/index.js
@@ -46,6 +46,24 @@ app.post('/sumar', (req, res) => { // http://localhost:3000/sumar
   res.send({ resultado });
 });
 
+
+//end point
+app.post('/area-triangulo', (req, res) => { // http://localhost:3000/sumar
+  const { base, altura } = req.body;// se declaran los datos de entrada
+
+  // Validar que se hayan enviado los dos números  que no esten vacio
+  if (!base || !altura) {
+    return res.status(400).send({ error: 'Faltan números para sumar' });
+  }
+
+  // Sumar los números
+  const resultado = base * altura /2;
+
+  // Enviar el resultado al front 
+
+  res.send({ resultado });
+});
+
 app.post('/restar', (req, res) => { // http://localhost:3000/sumar
   const { num1, num2 } = req.body;// se declaran los datos de entrada
 
